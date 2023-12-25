bb38460864bf83f3a920ecc02957e09947893b61
hintsocials <hints.socials@gmail.com>
2023-12-25 18:17:50 +0530

    2222

--- server.cjs
+++ server.cjs
@@ -17,7 +17,8 @@ admin.initializeApp({
 });
 
 const app = express();
-const port = 3001; // Set the port you want to use
+// const port = 3001; // Set the port you want to use
+const port = process.env.PORT || 3001;
 const storage = admin.storage().bucket(); 
 // Plivo configuration
 // const plivoClient = new plivo.Client('YOUR_PLIVO_API_KEY', 'YOUR_PLIVO_API_SECRET');
@@ -26,31 +27,39 @@ const storage = admin.storage().bucket();
 app.use(bodyParser.json({ limit: '10mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors({
-    origin: "https://prototype-mmi5.onrender.com", // Change this to your frontend URL
+    origin: "http://localhost:3000", // Change this to your frontend URL
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   }));
 
-app.use(cookieParser());
+
 
   // Use express-session middleware
+  // Use express-session middleware with Firebase Realtime Database session storage
+const FirebaseStore = require('connect-session-firebase')(session);
 app.use(session({
   secret: "1111", // Replace with a secret key for session encryption
-  resave: false,
+  resave: true,
   saveUninitialized: true,
+  store: new FirebaseStore({
+    database: admin.database(),
+    sessions: 'sessions', // Specify the node where sessions will be stored
+  }),
   cookie: {
-    secure: false, // Set to true in a production environment with HTTPS
+    secure: true, // Set to true in a production environment with HTTPS
     maxAge: 24 * 60 * 60 * 1000, // Session expires after 24 hours
+    sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // Adjust based on environment
   },
 }));
 app.use(express.json());
+app.use(cookieParser());
 
 // Middleware to log session information
-// app.use((req, res, next) => {
-//   console.log('Session ID:', req.sessionID);
-//   console.log('User ID:', req.session.userId);
-//   next();
-// });
+app.use((req, res, next) => {
+  console.log('Session ID:', req.session.id);
+  console.log('User ID:', req.session.userId);
+  next();
+});
 
 // Endpoint for generating OTP and storing user data
 app.post('/api/generate-otp', async (req, res) => {
@@ -67,7 +76,7 @@ app.post('/api/generate-otp', async (req, res) => {
     const apiKey = 'SayRwqio7DHnXj5zrtP9CUGpM2FvekV6hBfATJLdlQuI08xEs45CoxBJjcnmsi9FK4pzN6tk0ylhb2uQ';
 
     // Generate a three-digit user ID
-    // const userId = Math.floor(100 + Math.random() * 900);
+    const userId = Math.floor(100 + Math.random() * 900);
 
     // Set user data under the 'usersnew' node using push
     const userRef = admin.database().ref('usersnew');
@@ -79,42 +88,44 @@ app.post('/api/generate-otp', async (req, res) => {
     };
     await newUserRef.set(user);
 
-    // Send OTP via SMS OTP
-    const smsResponse = await unirest.get("https://www.fast2sms.com/dev/bulkV2")
-      .query({
-        "authorization": apiKey,
-        "variables_values": otp.toString(), // Convert OTP to string
-        "route": "otp",
-        "numbers": phone
-      })
-      .header("cache-control", "no-cache");
-      console.log('Fast2SMS API response:', smsResponse.body);
+    // // Send OTP via SMS OTP
+    // const smsResponse = await unirest.get("https://www.fast2sms.com/dev/bulkV2")
+    //   .query({
+    //     "authorization": apiKey,
+    //     "variables_values": otp.toString(), // Convert OTP to string
+    //     "route": "otp",
+    //     "numbers": phone
+    //   })
+    //   .header("cache-control", "no-cache");
+    //   console.log('Fast2SMS API response:', smsResponse.body);
 
-    if (smsResponse.error) {
-      // Failed to send OTP
-      console.error(`Failed to send OTP to ${phone}: ${smsResponse.error}`);
-      res.status(500).json({ success: false, error: 'Failed to send OTP' });
-      return;
-    }
+    // if (smsResponse.error) {
+    //   // Failed to send OTP
+    //   console.error(`Failed to send OTP to ${phone}: ${smsResponse.error}`);
+    //   res.status(500).json({ success: false, error: 'Failed to send OTP' });
+    //   return;
+    // }
 
-    // Check the response body for success
-    if (smsResponse.body.return === true) {
-      // OTP sent successfully
-      console.log(`OTP sent successfully to ${phone}`);
-    } else {
-      // Failed to send OTP
-      console.error(`Failed to send OTP to ${phone}: ${smsResponse.body.message}`);
-      res.status(500).json({ success: false, error: `Failed to send OTP: ${smsResponse.body.message}` });
-      return;
-    }
+    // // Check the response body for success
+    // if (smsResponse.body.return === true) {
+    //   // OTP sent successfully
+    //   console.log(`OTP sent successfully to ${phone}`);
+    // } else {
+    //   // Failed to send OTP
+    //   console.error(`Failed to send OTP to ${phone}: ${smsResponse.body.message}`);
+    //   res.status(500).json({ success: false, error: `Failed to send OTP: ${smsResponse.body.message}` });
+    //   return;
+    // }
 
-    // req.session.userId = user.userId;
-    // // console.log(`Generated OTP for ${phone}: ${otp}`);
+    req.session.userId = user.userId;
+    req.session.save();
+    // res.json({ success: true, userId: user.userId });
+    // console.log(`Generated OTP for ${phone}: ${otp}`);
     console.log(`Generated OTP for ${phone}: ${otp}`);
-    // console.log('Session ID after OTP generation:', req.sessionID);
-    console.log('User IDs after OTP generation:', user.userId);
+    console.log('Session ID after OTP generation:', req.session.id);
+    console.log('User ID after OTP generation:', req.session.userId);
 
-    res.json({ success: true, userId : user.userId , phone });
+    res.json({ success: true, userId: user.userId });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, error: 'Internal Server Error' });
@@ -125,10 +136,9 @@ app.post('/api/generate-otp', async (req, res) => {
 app.post('/api/validate-otp', async (req, res) => {
   try {
     //const { userId, enteredOtp } = req.body;
-    const { enteredOtp, userId } = req.body;
-
-
-    // console.log('Session ID during OTP validation:', req.sessionID);
+    const { enteredOtp } = req.body;
+    const userId = req.session.userId;
+    console.log('Session ID during OTP validation:', req.session.id);
     console.log('User ID during OTP validation:', userId);
     console.log('Entered OTP:', enteredOtp);
 
@@ -149,7 +159,7 @@ app.post('/api/validate-otp', async (req, res) => {
     // Validate OTP
     if (enteredOtp == user.otp) {
       await userRef.update({ otp: null });
-      return res.json({ success: true, message: 'OTP is valid',userId : userId });
+      return res.json({ success: true, message: 'OTP is valid' });
     } else {
       return res.json({ success: false, error: 'Invalid OTP' });
     }
@@ -162,15 +172,15 @@ app.post('/api/validate-otp', async (req, res) => {
 // Endpoint for saving user information
 app.post('/api/save-user-info', async (req, res) => {
   try {
-    const { name, dobsplit, gender, age,userId } = req.body;
-    
+    const { name, dobsplit, gender, age } = req.body;
+    const userId = req.session.userId;
     // Check if the user already exists in the database
     const userRef = admin.database().ref(`usersnew/${userId}`);
     const userSnapshot = await userRef.once('value');
     const existingUserData = userSnapshot.val();
     
 
-    console.log('Session ID during user-info:', req.sessionID);
+    console.log('Session ID during user-info:', req.session.id);
     // Assuming 'usersinfo' is the database node to store user information
     if (existingUserData) {
       // User exists, update the information
@@ -193,7 +203,7 @@ app.post('/api/save-user-info', async (req, res) => {
     console.log('User information saved for userId:', userId);
     // console.log('User information:', userRef);
 
-    res.json({ success: true, message: 'User information saved successfully',userId : userId });
+    res.json({ success: true, message: 'User information saved successfully' });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, error: 'Internal Server Error' });
@@ -203,8 +213,9 @@ app.post('/api/save-user-info', async (req, res) => {
 // Endpoint for saving user information after on boarding
 app.post('/api/save-user-infonew', async (req, res) => {
   try {
-    const { name,userId, dobsplit, gender, age, profileImageOne, profileImageTwo, qualification, bio, interests, statuses, preferedAge, preferedGender } = req.body;
-   // Check if the user already exists in the database
+    const { name, dobsplit, gender, age, profileImageOne, profileImageTwo, qualification, bio, interests, statuses, preferedAge, preferedGender } = req.body;
+    const userId = req.session.userId;
+    // Check if the user already exists in the database
     const userRef = admin.database().ref(`usersnew/${userId}`);
     const userSnapshot = await userRef.once('value');
     const existingUserData = userSnapshot.val();
@@ -253,7 +264,7 @@ app.post('/api/save-user-infonew', async (req, res) => {
   }
 
   console.log('User information saved for userId:', userId);
-  res.json({ success: true, message: 'User information saved successfully',userId : userId });
+  res.json({ success: true, message: 'User information saved successfully' });
 } catch (error) {
   console.error(error);
   res.status(500).json({ success: false, error: 'Internal Server Error' });
@@ -280,13 +291,12 @@ async function saveImageAndGetURL(userId, folderName, imageName, imageData) {
 //this is for fetchhing the profile
 app.get('/api/fetch-user-details', async (req, res) => {
   try {
-    const { userId } = req.body;
-
+    const userId = req.session.userId; // Retrieve user ID from the session
     const userRef = admin.database().ref(`usersnew/${userId}`);
     const userSnapshot = await userRef.once('value');
     const user = userSnapshot.val();
 
-    console.log('Session ID during profiling:', req.sessionID);
+    console.log('Session ID during profiling:', req.session.id);
     console.log('User ID during OTP profiling:', userId);
 
     if (!user) {
@@ -294,7 +304,7 @@ app.get('/api/fetch-user-details', async (req, res) => {
     }
 
     // Return the user details to the client
-    return res.json({ success: true, user,userId : userId });
+    return res.json({ success: true, user });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, error: 'Internal Server Error' });
@@ -304,8 +314,7 @@ app.get('/api/fetch-user-details', async (req, res) => {
 // this is for rendering the hint-landing page whhich shows all users except the current user in the venue
 app.get('/api/users', async (req, res) => {
   try {
-    const { userId } = req.query;
-    const currentUserId = userId; // Retrieve the current user's ID from the session
+    const currentUserId = req.session.userId; // Retrieve the current user's ID from the session
 
     // Retrieve all users from the 'usersnew' node
     const usersSnapshot = await admin.database().ref('usersnew').once('value');
@@ -317,7 +326,7 @@ app.get('/api/users', async (req, res) => {
     // Filter out the current user
     const filteredUsers = Object.values(usersObject).filter(user => user.userId !== currentUserId);
 
-    res.json({ success: true, users: filteredUsers,userId : userId });
+    res.json({ success: true, users: filteredUsers });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, error: 'Internal Server Error' });
