0c71c71649ff0faf1ed6fbb8bd88650e1359d8c6
hintsocials <hints.socials@gmail.com>
2023-12-25 17:23:44 +0530

    f5

--- server.cjs
+++ server.cjs
@@ -29,8 +29,6 @@ app.use(cors({
     origin: "https://prototype-mmi5.onrender.com", // Change this to your frontend URL
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
-    optionsSuccessStatus: 204, // Some legacy browsers choke on 204
-    allowedHeaders: "Content-Type, Authorization", // Add other headers if needed
   }));
 
 app.use(cookieParser());
